24928b4628a2c62669cbaf6c1bd4103069dfe4f3
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2025-10-01 16:55:49 +0300

    feat: add static HTML serving and create index.html for Meal’n’Nuts API

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,11 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = Number(process.env.PORT || 4000);
@@ -10,9 +15,10 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (_req, res) => {
-  res.json({ message: "Hello from Eetu, Bror and Julius!" });
-});
+// Serve static HTML in /public
+app.use(express.static(path.join(__dirname, "./public")));
+
+// Health endpoint
 app.get("/health", (_req, res) => {
   try {
     res.json({ status: "ok" });
