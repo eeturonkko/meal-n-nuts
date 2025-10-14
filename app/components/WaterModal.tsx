d34999d0c492812e6d939ecc0f6e6a3836ec4386
Zaffot <Bror.torstensson@student.hamk.fi>
2025-10-14 20:25:31 +0300

    water: add modal route and WaterModal component (with progress circle)

---
+++ app/components/WaterModal.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import COLORS from "../utils/constants";
+import { NutrientProgressCircle } from "./NutrientProgressCircle";
+
